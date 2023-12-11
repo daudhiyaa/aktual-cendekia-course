8e313c0f21392773bc8e54ae53e0ec760650b7f0
dilwdn <dillawahdana12@gmail.com>
2023-12-11 23:52:18 +0700

    update file profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -1,34 +1,35 @@
 <?php
 
-   include '../components/connect.php';
+include '../components/connect.php';
 
-   if(isset($_COOKIE['tutor_id'])){
-      $tutor_id = $_COOKIE['tutor_id'];
-   }else{
-      $tutor_id = '';
-      header('location:login.php');
-   }
+if (isset($_COOKIE['tutor_id'])) {
+   $tutor_id = $_COOKIE['tutor_id'];
+} else {
+   $tutor_id = '';
+   header('location:login.php');
+}
 
-   $select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
-   $select_playlists->execute([$tutor_id]);
-   $total_playlists = $select_playlists->rowCount();
+$select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
+$select_playlists->execute([$tutor_id]);
+$total_playlists = $select_playlists->rowCount();
 
-   $select_contents = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ?");
-   $select_contents->execute([$tutor_id]);
-   $total_contents = $select_contents->rowCount();
+$select_contents = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ?");
+$select_contents->execute([$tutor_id]);
+$total_contents = $select_contents->rowCount();
 
-   $select_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ?");
-   $select_likes->execute([$tutor_id]);
-   $total_likes = $select_likes->rowCount();
+$select_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ?");
+$select_likes->execute([$tutor_id]);
+$total_likes = $select_likes->rowCount();
 
-   $select_comments = $conn->prepare("SELECT * FROM `comments` WHERE tutor_id = ?");
-   $select_comments->execute([$tutor_id]);
-   $total_comments = $select_comments->rowCount();
+$select_comments = $conn->prepare("SELECT * FROM `comments` WHERE tutor_id = ?");
+$select_comments->execute([$tutor_id]);
+$total_comments = $select_comments->rowCount();
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -42,46 +43,47 @@
    <link rel="stylesheet" href="../css/admin_style.css">
 
 </head>
+
 <body>
 
-<?php include '../components/admin_header.php'; ?>
-   
-<section class="tutor-profile" style="min-height: calc(100vh - 19rem);"> 
+   <?php include '../components/admin_header.php'; ?>
 
-   <h1 class="heading">profile details</h1>
+   <section class="tutor-profile" style="min-height: calc(100vh - 19rem);">
 
-   <div class="details">
-      <div class="tutor">
-         <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
-         <h3><?= $fetch_profile['name']; ?></h3>
-         <span><?= $fetch_profile['profession']; ?></span>
-         <a href="update.php" class="inline-btn">Update Profile</a>
+      <h1 class="heading">Profile Details</h1>
+
+      <div class="details">
+         <div class="tutor">
+            <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+            <h3><?= $fetch_profile['name']; ?></h3>
+            <span><?= $fetch_profile['profession']; ?></span>
+            <a href="update.php" class="inline-btn">Update Profile</a>
+         </div>
+         <div class="flex">
+            <div class="box">
+               <span><?= $total_playlists; ?></span>
+               <p>Total Playlists</p>
+               <a href="playlists.php" class="btn">View Playlists</a>
+            </div>
+            <div class="box">
+               <span><?= $total_contents; ?></span>
+               <p>Total Videos</p>
+               <a href="contents.php" class="btn">View Contents</a>
+            </div>
+            <div class="box">
+               <span><?= $total_likes; ?></span>
+               <p>Total Likes</p>
+               <a href="contents.php" class="btn">View Contents</a>
+            </div>
+            <div class="box">
+               <span><?= $total_comments; ?></span>
+               <p>Total Comments</p>
+               <a href="comments.php" class="btn">View Comments</a>
+            </div>
+         </div>
       </div>
-      <div class="flex">
-         <div class="box">
-            <span><?= $total_playlists; ?></span>
-            <p>total playlists</p>
-            <a href="playlists.php" class="btn">view playlists</a>
-         </div>
-         <div class="box">
-            <span><?= $total_contents; ?></span>
-            <p>total videos</p>
-            <a href="contents.php" class="btn">view contents</a>
-         </div>
-         <div class="box">
-            <span><?= $total_likes; ?></span>
-            <p>total likes</p>
-            <a href="contents.php" class="btn">view contents</a>
-         </div>
-         <div class="box">
-            <span><?= $total_comments; ?></span>
-            <p>total comments</p>
-            <a href="comments.php" class="btn">view comments</a>
-         </div>
-      </div>
-   </div>
 
-</section>
+   </section>
 
 
 
@@ -97,9 +99,10 @@
 
 
 
-<?php include '../components/footer.php'; ?>
+   <?php include '../components/footer.php'; ?>
 
-<script src="../js/admin_script.js"></script>
+   <script src="../js/admin_script.js"></script>
 
 </body>
+
 </html>
